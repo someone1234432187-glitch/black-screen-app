5e6f19e4005f0cecf2032566e262900ae59cb6b9
someone1234432187-glitch <someone1234432187@gmail.com>
2025-10-18 01:23:07 -0400

    Update WindowInitiator.cpp

--- src/app/WindowInitiator.cpp
+++ src/app/WindowInitiator.cpp
@@ -1,45 +1,25 @@
-//
-// Created by spoil on 04/14/2024.
-//
-
 #include "WindowInitiator.hpp"
+#include <windows.h>
 
-#include <algorithm>
-#include <regex>
-#include <utility>
-
-#include "ColorHandler.hpp"
-
-LRESULT CALLBACK HandleWindowMessages(HWND windowHandle, UINT messageType, WPARAM windowParameterValue, LPARAM messageData);
-
-std::string WindowInitiator::m_color = "black";
-bool WindowInitiator::disableKeyExit = false;
-
-WindowInitiator::WindowInitiator(std::string color, const bool& disableKeyExit) {
-    WindowInitiator::disableKeyExit = disableKeyExit;
-
-    std::ranges::transform(color, color.begin(),
-                           [](const unsigned char stringCharacter) { return std::tolower(stringCharacter); });
-
-    m_color = color == "black" ? std::move(color) :
-              ColorHandler::colorMap.contains(color) ? ColorHandler::colorMap[color] : std::move(color);
-}
+LRESULT CALLBACK HandleWindowMessages(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 void WindowInitiator::createWindow() {
-    const WNDCLASS windowClass = {
-        .lpfnWndProc = HandleWindowMessages,
-        .hInstance = GetModuleHandle(nullptr),
-        .lpszClassName = "BlackWindowClass"
-    };
-    RegisterClass(&windowClass);
+    const char* windowTitle = "My Custom Black Window"; // <-- Change your window title here
+    const char* className = "BlackWindowClass";
 
-    const auto windowHandle = CreateWindowEx(
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = HandleWindowMessages;
+    wc.hInstance = GetModuleHandle(nullptr);
+    wc.lpszClassName = className;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
         0,
-        "BlackWindowClass",
-        "Black Screen Application",
+        className,
+        windowTitle,
         WS_POPUP | WS_VISIBLE,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
+        0, 0,
         GetSystemMetrics(SM_CXSCREEN),
         GetSystemMetrics(SM_CYSCREEN),
         nullptr,
@@ -48,65 +28,33 @@ void WindowInitiator::createWindow() {
         nullptr
     );
 
-    ShowCursor(false);
+    ShowCursor(false);  // Hide cursor
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
 
-    ShowWindow(windowHandle, SW_SHOW);
-    UpdateWindow(windowHandle);
-
-    MSG message;
-    while (GetMessage(&message, nullptr, 0, 0)) {
-        TranslateMessage(&message);
-        DispatchMessage(&message);
-
-        if (message.message == WM_QUIT) {
-            break;
-        }
+    MSG msg;
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
-
-    UnregisterClass("BlackWindowClass", GetModuleHandle(nullptr));
 }
 
-LRESULT CALLBACK HandleWindowMessages(const HWND windowHandle, const UINT messageType, const WPARAM windowParameterValue, const LPARAM messageData) { // NOLINT(*-misplaced-const)
-    switch (messageType) {
+LRESULT CALLBACK HandleWindowMessages(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
         case WM_CLOSE:
+        case WM_KEYDOWN:
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
-        case WM_KEYDOWN: {
-            if (!WindowInitiator::disableKeyExit) {
-                PostQuitMessage(0);
-            }
-            return 0;
-        }
+
         case WM_ERASEBKGND: {
             RECT clientRect;
-            GetClientRect(windowHandle, &clientRect);
-            HBRUSH colorBrush;
-
-            if (WindowInitiator::m_color == "black") {
-                colorBrush = static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH));
-            } else {
-                if (std::regex_match(WindowInitiator::m_color, std::regex("^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$"))) {
-                    auto [red, green, blue] = ColorHandler::convertHextoRGB(WindowInitiator::m_color);
-                    colorBrush = CreateSolidBrush(RGB(red, green, blue));
-                } else {
-                    const auto errorMessage = "Invalid color argument. Expected a hex color code or color name.";
-                    MessageBox(nullptr, errorMessage, "Error - Black Screen Application", MB_ICONERROR | MB_OK);
-                    throw std::invalid_argument(errorMessage);
-                }
-            }
-
-            FillRect(reinterpret_cast<HDC>(windowParameterValue), &clientRect, colorBrush);
-
-            if (WindowInitiator::m_color != "black") {
-                DeleteObject(colorBrush);
-            }
-
+            GetClientRect(hwnd, &clientRect);
+            FillRect(reinterpret_cast<HDC>(wParam), &clientRect, static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH)));
             return 1;
         }
+
         default:
-            return DefWindowProc(windowHandle, messageType, windowParameterValue, messageData);
+            return DefWindowProc(hwnd, msg, wParam, lParam);
     }
 }
-
-
