a17c7c7f6fa7135e4da817d05f6db62e157de90d
someone1234432187-glitch <someone1234432187@gmail.com>
2025-10-18 01:22:48 -0400

    Update WindowInitiator.hpp

--- src/app/WindowInitiator.hpp
+++ src/app/WindowInitiator.hpp
@@ -1,21 +1,10 @@
-//
-// Created by spoil on 04/14/2024.
-//
-
 #ifndef WINDOWINITIATOR_HPP
 #define WINDOWINITIATOR_HPP
 
-#include <string>
-#include <windows.h>
-
 class WindowInitiator {
 public:
-    static std::string m_color;
-    static bool disableKeyExit;
-
-    explicit WindowInitiator(std::string color, const bool& disableKeyExit);
-    void createWindow();
+    static void createWindow(); // Creates the full-screen black window
 };
 
+#endif // WINDOWINITIATOR_HPP
 
-#endif //WINDOWINITIATOR_HPP
